ce1118519b45dc5fe91ca8d1f8a054cc63e6c521
Ksanork <miki11@poczta.onet.pl>
2016-02-08 00:37:11 +0100

    Hello

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-});
+}).listen(8080);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
