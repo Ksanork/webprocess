dca22f0553fed651586d87c766bd4bb2240f30d2
Ksanork <miki11@poczta.onet.pl>
2016-02-07 14:03:24 +0100

    Hello

--- server.js
+++ server.js
@@ -1,56 +1,8 @@
-var WebSocketServer = require('ws').Server;
-var http = require('http');
-
-var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080
-
-
-/*var io = require('socket.io').listen(16000,ipaddr);
-io.sockets.on('connection', function (socket) {
-  console.log('io connection');
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-console.log('io listening on port 16000');
-*/  
-var server = http.createServer();
-//var wss = new WebSocketServer({server: server, path: '/connect'});
-
-var  wss = new WebSocketServer({server:server})
-console.log(wss);
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({port: 8080});
 wss.on('connection', function(ws) {
-    console.log('/connection connected');
-    ws.on('message', function(data, flags) {
-        if (flags.binary) { return; }
-        console.log('>>> ' + data);
-        if (data == 'test') { console.log('test'); ws.send('got test'); }
-        if (data == 'hello') { console.log('hello'); ws.send('WAZZZUP!'); }
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
     });
-    ws.on('close', function() {
-      console.log('Connection closed!');
-    });
-    ws.on('error', function(e) {
-      console.log(e);
-    });
+    ws.send('something');
 });
-
-wss.on('connect', function(ws) {
-    console.log('/connect connected');
-    ws.on('message', function(data, flags) {
-        if (flags.binary) { return; }
-        console.log('>>> ' + data);
-        if (data == 'test') { console.log('test'); ws.send('got test'); }
-        if (data == 'hello') { console.log('hello'); ws.send('WAZZZUP!'); }
-    });
-    ws.on('close', function() {
-      console.log('Connection closed!');
-    });
-    ws.on('error', function(e) {
-      console.log(e);
-    });
-});
-
-console.log('Listening at IP ' + ipaddr +' on port '+port);
-server.listen(port,ipaddr);
