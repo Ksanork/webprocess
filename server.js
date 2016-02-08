fb64d9423d68e93049f48e23d637c31f2fa2e2f3
Ksanork <miki11@poczta.onet.pl>
2016-02-08 20:51:45 +0100

    WebProcess

--- server.js
+++ server.js
@@ -1,49 +1,11 @@
-var WebSocketServer = require('ws').Server;
-var http = require('http');
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send("Hello");
+});
 
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
-var server = http.createServer();
-
-app.get('/', function(req, res){
-  res.send("Hello");
-});
-
-/*
-var  wss = new WebSocketServer({server:server})
-console.log(wss);
-wss.on('connection', function(ws) {
-    console.log('/connection connected');
-    ws.on('message', function(data, flags) {
-        if (flags.binary) { return; }
-        console.log('>>> ' + data);
-        ws.send('WAZZZUP! from fucking Openshift');
-    });
-    ws.on('close', function() {
-      console.log('Connection closed!');
-    });
-    ws.on('error', function(e) {
-      console.log(e);
-    });
-});
-
-wss.on('connect', function(ws) {
-    console.log('/connect connected');
-    ws.on('message', function(data, flags) {
-        if (flags.binary) { return; }
-        console.log('>>> ' + data);
-        if (data == 'test') { console.log('test'); ws.send('got test'); }
-        if (data == 'hello') { console.log('hello'); ws.send('WAZZZUP!'); }
-    });
-    ws.on('close', function() {
-      console.log('Connection closed!');
-    });
-    ws.on('error', function(e) {
-      console.log(e);
-    });
-});
-
-console.log('Listening at IP ' + ipaddr +' on port '+port);
-server.listen(port,ipaddr);*/
+app.listen(80);
