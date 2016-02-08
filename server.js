1184c42522807abf33a8d54fbcffab6288774d15
Ksanork <miki11@poczta.onet.pl>
2016-02-08 21:59:54 +0100

    WebProcess

--- server.js
+++ server.js
@@ -3,22 +3,15 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var WebSocketServer = require('ws').Server
 var http = require('http');
-var express = require('express');
-var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+//browser
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+      
+      response.write("Welcome to Node.js on OpenShift!\n\n");
+      response.end("Thanks for visiting us! \n");
 });
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-  
-  
-});
-
 
 //websocket app
 wss = new WebSocketServer({
