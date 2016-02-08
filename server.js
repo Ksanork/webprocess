1ef8b81eb93e08197446e8b35d087b8801213f9e
Ksanork <miki11@poczta.onet.pl>
2016-02-08 23:36:35 +0100

    WebProcess

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({port: 8081});
 var http = require("http");
-var sys = require("sys");
 
 var server = http.createServer(function(request,response){
   response.writeHeader(200, {"Content-Type": "text/plain"});
@@ -9,7 +8,7 @@ var server = http.createServer(function(request,response){
   if (request.url == "/"){
     for (var i = 0; i < wss.client.length; i++) {
       var ws = wss.clients[i];
-      sys.puts("sent msg");
+      //sys.puts("sent msg");
       ws.send("photo plox");
     }
   }
