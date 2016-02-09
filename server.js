bea1511c9dbabc32ac48782dad4312e5c9bf5306
Ksanork <miki11@poczta.onet.pl>
2016-02-09 15:56:45 +0100

    WebProcess

--- server.js
+++ server.js
@@ -1,38 +1,60 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = 8000;
+var serverUrl = "127.0.0.1";
+
+var http = require("http");
 var path = require("path"); 
-var fs = require("fs"); 
+var fs = require("fs");         
 
-var WebSocketServer = require('ws').Server
-var http = require('http');
+console.log("Starting web server at " + serverUrl + ":" + port);
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    var server = http.createServer(function(request, response) {
-        console.log((new Date()) + ' Received request for ' + request.url);
-        response.writeHead(200, {'Content-Type': 'text/html'});
-          response.write(html);
-          response.end("Thanks for visiting us! \n");
-    });
-    
-    server.listen( port, ipaddress, function() {
-        console.log((new Date()) + ' Server is listening on port 8080');
-    });
-    
-    var wss = new WebSocketServer({
-        server: server,
-        autoAcceptConnections: false
-    });
-    wss.on('connection', function(ws) {
-      console.log("New connection");
-      ws.on('message', function(message) {
-        ws.send("Received: " + message);
-      });
-      ws.send('Welcome!');
-    });
-    
-    console.log("Listening to " + ipaddress + ":" + port + "...");
-});
+http.createServer( function(req, res) {
 
+    var now = new Date();
+
+    var filename = req.url || "index.html";
+    var ext = path.extname(filename);
+    var localPath = __dirname;
+    var validExtensions = {
+        ".html" : "text/html",          
+        ".js": "application/javascript", 
+        ".css": "text/css",
+        ".txt": "text/plain",
+        ".jpg": "image/jpeg",
+        ".gif": "image/gif",
+        ".png": "image/png"
+    };
+    var isValidExt = validExtensions[ext];
+
+    if (isValidExt) {
+        
+        localPath += filename;
+        fs.exists(localPath, function(exists) {
+            if(exists) {
+                console.log("Serving file: " + localPath);
+                getFile(localPath, res, ext);
+            } else {
+                console.log("File not found: " + localPath);
+                res.writeHead(404);
+                res.end();
+            }
+        });
+
+    } else {
+        console.log("Invalid file extension detected: " + ext)
+    }
+
+}).listen(port, serverUrl);
+
+function getFile(localPath, res, mimeType) {
+    fs.readFile(localPath, function(err, contents) {
+        if(!err) {
+            res.setHeader("Content-Length", contents.length);
+            res.setHeader("Content-Type", mimeType);
+            res.statusCode = 200;
+            res.end(contents);
+        } else {
+            res.writeHead(500);
+            res.end();
+        }
+    });
+}
