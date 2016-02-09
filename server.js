e3ebc884d7a07258938671b9baa0abef02703426
Ksanork <miki11@poczta.onet.pl>
2016-02-09 14:54:07 +0100

    WebProcess

--- server.js
+++ server.js
@@ -9,21 +9,10 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 //app.listen(80, ipaddress);
 
 var WebSocketServer = require('ws').Server
-var http = require('http');
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-      response.write("Welcome to Node.js on OpenShift!\n\n");
-      response.end("Thanks for visiting us! \n");
-});
-
-server.listen( port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
 
 wss = new WebSocketServer({
-    server: server,
+    port: port,
     autoAcceptConnections: false
 });
 wss.on('connection', function(ws) {
