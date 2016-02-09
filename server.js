08339988112ae2833c90eca0e978212d482a6dac
Ksanork <miki11@poczta.onet.pl>
2016-02-09 14:03:23 +0100

    WebProcess

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var ext = /[\w\d_-]+\.[\w\d]+$/;
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
-http.createServer(function(req, res){
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('index.html').pipe(res);
-    } else if (ext.test(req.url)) {
-        fs.exists(path.join(__dirname, req.url), function (exists) {
-            if (exists) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                fs.createReadStream('index.html').pipe(res);
-            } else {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                fs.createReadStream('404.html').pipe(res);
-        });
-    } else {
-        //  add a RESTful service
-    }
-}).listen(8000);
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3000);
+
+
+
+console.log("Running at Port 3000");
