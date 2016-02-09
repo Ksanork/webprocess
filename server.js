dde7c3479096d72947b7af678afbee30ce3c4579
Ksanork <miki11@poczta.onet.pl>
2016-02-09 15:16:14 +0100

    WebProcess

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var path = require("path"); 
+var fs = require("fs"); 
 
 var WebSocketServer = require('ws').Server
 var http = require('http');
