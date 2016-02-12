4db5f1b3cb6958f481e3862f2627b58a2546ee3c
Ksanork <miki11@poczta.onet.pl>
2016-02-12 15:53:24 +0100

    WebProcess

--- models/server.js
+++ models/server.js
@@ -21,33 +21,58 @@ var Server = module.exports = {
         //zdarzenie reagujące na połączenie z klientem
         this.ws.on('connection', function(ws) {
           console.log("new connection");
+          t.handleConnect(ws);
           
           //zdarzenie reagujące na przychodzące wiadomości
           ws.on('message', function(message) {
             t.handleIncoming(this, message);
           });
           
+          //zdarzenie reagujące na zamykanie połączenia
           ws.on('close', function(message) {
-            console.log('disconnect');
+             t.handleDisconnect(ws, message);
           });
         });
     },
     
     //dodanie klienta do listy - AUTORYZACJA?
-    addClient: function(name) {
+    addClient: function(name, ws) {
         console.log("new client");
+        console.log("ilosć klientów " + this.clients.length);
         this.clients.push({
-            'name': name    
+            'name': name,
+            'ws': ws
           });  
+    },
+    
+    //usuwa klienta na podstawie obiektu socketu
+    removeClient: function(ws) {
+        for(var i = 0; i < this.clients.length; i++) {
+            if(ws == this.clients[i].ws) 
+                this.clients.splice(i, 1);
+        }
     },
     
     sendTo: function(ws, message) {
         ws.send(message);
     },
     
+    //zwraca tylko this.clients bez ws
+    getClientsNames: function() {
+        var result = new Array();
+        
+        for(var i = 0; i < this.clients.length; i++) {
+            result.push({
+                'name': this.clients[i].name,
+            }); 
+        }
+            
+        return result;  
+    },
+    
     //dodać pattern?
     sendJSON: function(ws, type, content) {
-        console.log(this.clients);
+        //console.log(this.clients);
         
         var prepjson = {
             'type': type,
@@ -58,5 +83,7 @@ var Server = module.exports = {
     },
    
     //do przykrycia
-    handleIncoming: function(ws, message) {}
+    handleConnect: function(ws) {},
+    handleIncoming: function(ws, message) {},
+    handleDisconnect: function(ws, message) {}
 };
